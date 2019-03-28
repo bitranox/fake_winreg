05a1606aec02c1c5d91830d9ffa5e543ea32b79e
Robert Nowotny <bitranox@users.noreply.github.com>
2019-03-28 21:51:19 +0100

    remove unused imports

--- lib_registry/tests/test_lib_registry.py
+++ lib_registry/tests/test_lib_registry.py
@@ -1,9 +1,9 @@
 """lib_registry tests"""
 
 from lib_registry import *
-import pytest
 from winreg import *
 
 
 def test_get_number_of_subkeys():
-    assert get_number_of_subkeys(HKEY_USERS) >= 1
+    result = get_number_of_subkeys(HKEY_USERS)
+    assert isinstance(result, int)
