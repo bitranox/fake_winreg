84afb9fc19a71ce79cea1ee6e24c579be6decfea
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-05 19:25:07 +0200

    make setup.py test working

--- __init__.py
+++ __init__.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from .lib_registry.lib_registry import *
+# this is only for local development when the package is actually not installed
+try:
+    from .lib_registry.lib_registry import *
+except Exception:
+    pass
 
 __title__ = 'lib_registry'
 __version__ = '1.0.1'
