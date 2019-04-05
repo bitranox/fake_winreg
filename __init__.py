f7f9e8f80a74dc83c3094192ef8075042cdccc7e
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-05 20:20:52 +0200

    update wine testing

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@
 # this is only for local development when the package is actually not installed
 try:
     from .lib_registry.lib_registry import *
-except Exception:
+except ImportError:
     pass
 
 __title__ = 'lib_registry'
