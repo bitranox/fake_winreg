2717fd820e5fa2b7bfc2295c3130d90e774d1423
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-08 00:54:21 +0200

    update tests

--- lib_registry/tests/test_lib_registry.py
+++ lib_registry/tests/test_lib_registry.py
@@ -6,3 +6,12 @@ from lib_registry import *
 def test_get_number_of_subkeys():
     result = get_number_of_subkeys(HKEY_USERS)
     assert isinstance(result, int)
+
+
+def test_get_ls_user_sids():
+    ls_user_sids = get_ls_user_sids()
+    username = get_username_from_sid(ls_user_sids[1])
+    if get_is_platform_windows_wine():
+        assert username
+    else:
+        assert username == 'systemprofile'
