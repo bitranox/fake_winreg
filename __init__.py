9b6c55102f2531f12a719fdf09066d68c3bd7423
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-07 22:55:23 +0200

    update __init__.py for local package development

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
+import logging
 
-# this is only for local development when the package is actually not installed
 try:
     from .lib_registry.lib_registry import *
 except ImportError:
-    pass
+    logger = logging.getLogger()
+    logger.debug('Import Error - this __init__.py is only meant for local package development')
 
 __title__ = 'lib_registry'
 __version__ = '1.0.1'
