e1c01c88c76a729cbd88a39744afd726c0f5f12c
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-12 21:26:39 +0200

    wine installer - create permanent path to python version

--- install_wine.sh
+++ install_wine.sh
@@ -17,10 +17,13 @@ wget -nc https://dl.winehq.org/wine-builds/winehq.key
 sudo apt-key add winehq.key
 sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ xenial main'
 echo "Wine Packages Update"
-sudo apt-get -y update
-sudo apt-get install -y cabextract
+sudo apt-get update
 echo "Wine Packages Install"
 sudo apt-get install --install-recommends winehq-${wine_version}
+sudo apt-get install -y cabextract
+sudo apt-get install -y libxml2
+sudo apt-get install -y libpng
+
 echo "Install latest Winetricks"
 cd /usr/bin
 sudo rm -f winetricks
@@ -30,3 +33,8 @@ sudo winetricks -q --self-update
 echo "Install latest Winetricks - done"
 
 cd ${save_path}
+
+echo "******************************************************************************************************************"
+echo "******************************************************************************************************************"
+echo "FINISHED installing WINE and WINETRICKS"
+echo "******************************************************************************************************************"
