7d1c90f7189059e1546c1a6d74554bd1a6df90fb
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-15 15:52:04 +0200

    refractor

--- __init__.py
+++ __init__.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
-import logging
 
-try:
-    from .lib_registry.lib_registry import *
-except ImportError:
-    logger = logging.getLogger()
-    logger.debug('Import Error - this __init__.py is only meant for local package development')
+# this __init__.py is only meant for local package development
+from .lib_registry import *
 
 __title__ = 'lib_registry'
 __version__ = '1.0.1'
