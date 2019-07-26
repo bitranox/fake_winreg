6338311ca19dffe053d2c1dd6095cce093013773
bitranox <rnowotny1966@gmail.com>
2019-07-26 23:54:15 +0200

    refractor

--- lib_registry/tests/test_lib_registry.py
+++ lib_registry/tests/test_lib_registry.py
@@ -2,18 +2,26 @@
 
 from lib_registry import *
 
+if is_platform_windows:
+    if is_python2:
+        import _winreg as winreg    # type: ignore
+    else:
+        import winreg               # type: ignore
+
 
 def test_get_number_of_subkeys():
     # type: () -> None
-    result = get_number_of_subkeys(winreg.HKEY_USERS)
-    assert isinstance(result, int)
+    if is_platform_windows:
+        result = get_number_of_subkeys(winreg.HKEY_USERS)
+        assert isinstance(result, int)
 
 
 def test_get_ls_user_sids():
     # type: () -> None
-    ls_user_sids = get_ls_user_sids()
-    username = get_username_from_sid(ls_user_sids[1])
-    if get_is_platform_windows_wine():
-        assert username
-    else:
-        assert username == 'systemprofile'
+    if is_platform_windows:
+        ls_user_sids = get_ls_user_sids()
+        username = get_username_from_sid(ls_user_sids[1])
+        if get_is_platform_windows_wine():
+            assert username
+        else:
+            assert username == 'systemprofile'
