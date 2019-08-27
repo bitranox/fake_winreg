192c1e31c10c1bf1ca0faa28389d62b47f4c91f2
bitranox <rnowotny1966@gmail.com>
2019-08-27 20:26:17 +0200

    refractor

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 # this __init__.py is only meant for local package development
-from .lib_registry import *
-
-__title__ = 'lib_registry'
-__version__ = '1.0.1'
-__name__ = 'lib_registry'
+try:
+    from .lib_registry import *
+# this we need for pip install --install-option test
+except ImportError:
+    import lib_registry
--- lib_registry/__init__.py
+++ lib_registry/__init__.py
@@ -1,7 +1,13 @@
-# -*- coding: utf-8 -*-
-
+import pathlib
 from .lib_registry import *
 
+
+def get_version() -> str:
+    with open(pathlib.Path(__file__).parent / 'version.txt', mode='r') as version_file:
+        version = version_file.readline()
+    return version
+
+
 __title__ = 'lib_registry'
-__version__ = '1.0.3'
+__version__ = get_version()
 __name__ = 'lib_registry'
