2fb17e9aa960d499b8d5712a9f34b3681b925beb
bitranox <rnowotny1966@gmail.com>
2019-08-27 22:46:29 +0200

    refractor

--- lib_registry/__init__.py
+++ lib_registry/__init__.py
@@ -2,7 +2,7 @@ import pathlib
 from .lib_registry import *
 
 
-def get_version() -> str:
+def get_version():      # type: ignore
     with open(pathlib.Path(__file__).parent / 'version.txt', mode='r') as version_file:
         version = version_file.readline()
     return version
