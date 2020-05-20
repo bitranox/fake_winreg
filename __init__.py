ad2f33283538fd3c52d19116050a7ec0b0d1beeb
bitranox <rnowotny1966@gmail.com>
2020-05-21 00:52:06 +0200

    refractor

--- __init__.py
+++
@@ -1,6 +0,0 @@
-# this __init__.py is only meant for local package development
-try:
-    from .lib_registry import *
-# this we need for pip install --install-option test
-except ImportError:
-    import lib_registry
