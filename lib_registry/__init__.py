7288ada513bf8947f7cfd52b81602ea8e89b5ef1
bitranox <rnowotny1966@gmail.com>
2020-06-07 14:26:24 +0200

    refractor

--- lib_registry/__init__.py
+++ lib_registry/__init__.py
@@ -1,5 +1,7 @@
-from . import __init__conf__
+from .lib_registry import *
 
+# this needs to come after the module imports, otherwise circular import under windows
+from . import __init__conf__
 __title__ = __init__conf__.title
 __version__ = __init__conf__.version
 __name__ = __init__conf__.name
@@ -7,5 +9,3 @@ __url__ = __init__conf__.url
 __author__ = __init__conf__.author
 __author_email__ = __init__conf__.author_email
 __shell_command__ = __init__conf__.shell_command
-
-from .lib_registry import *
